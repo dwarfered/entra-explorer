cf8c3ade005ef5af2e8ff64709a7a9c5afc5d582
Chris Dymond <dwarfered@users.noreply.github.com>
2025-01-01 22:26:32 +0800

    Create sample-data.ts

---
+++ components/app-registrations/sample-data.ts
@@ -0,0 +1,65 @@
+import { GridItem } from "./data-model";
+
+export const sampleItems: GridItem[] = [
+  {
+    appId: "abcdefg",
+    displayName: "My App",
+    activeKeyCredentials: [{
+      key: "",
+      endDateTime: "",
+      customKeyIdentifier: "",
+      usage: "",
+      keyId: "",
+      displayName: "",
+      type: "",
+      startDateTime: ""
+    }],
+    activePasswordCredentials: [],
+    expiredKeyCredentials: [],
+    expiredPasswordCredentials: [],
+    longLivedKeyCredentials: [],
+    longLivedPasswordCredentials: [],
+    warningKeyCredentials: [],
+    warningPasswordCredentials: [],
+  },
+  {
+    appId: "abcdefgh",
+    displayName: "Dev App",
+    activeKeyCredentials: [{
+      key: "",
+      endDateTime: "",
+      customKeyIdentifier: "",
+      usage: "",
+      keyId: "",
+      displayName: "",
+      type: "",
+      startDateTime: ""
+    },{
+      key: "",
+      endDateTime: "",
+      customKeyIdentifier: "",
+      usage: "",
+      keyId: "",
+      displayName: "",
+      type: "",
+      startDateTime: ""
+    }
+  ],
+    activePasswordCredentials: [],
+    expiredKeyCredentials: [{
+      key: "",
+      endDateTime: "",
+      customKeyIdentifier: "",
+      usage: "",
+      keyId: "",
+      displayName: "",
+      type: "",
+      startDateTime: ""
+    }],
+    expiredPasswordCredentials: [],
+    longLivedKeyCredentials: [],
+    longLivedPasswordCredentials: [],
+    warningKeyCredentials: [],
+    warningPasswordCredentials: [],
+  },
+];
