745a639631e7e927558533dbad32725b604ccbc0
Chris Dymond <dwarfered@users.noreply.github.com>
2025-01-01 14:08:22 +0800

    Update page.tsx

---
+++ app/app-registrations/credential-status/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbButton,
+  BreadcrumbDivider,
+  Title3,
+  makeStyles,
+  shorthands,
+  Body1,
+} from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  toolbar: {
+    ...shorthands.margin("6px"),
+  },
+});
+
+export default function CredentialStatus() {
+  const styles = useStyles();
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <BreadcrumbButton>Application Registrations</BreadcrumbButton>
+        </BreadcrumbItem>
+        <BreadcrumbDivider />
+        <BreadcrumbItem>
+          <BreadcrumbButton current>Credential Status</BreadcrumbButton>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Title3 className={styles.toolbar}>Credential Status</Title3>
+      {/* <CredentialStatusGrid /> */}
+    </>
+  );
+}
