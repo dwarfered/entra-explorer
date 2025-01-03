e8cf3a969d7f60d2b52ecb49f5101f05a7160f41
Chris Dymond <dwarfered@users.noreply.github.com>
2025-01-03 19:21:34 +0800

    Create SamlStatusGrid.data-model.ts

---
+++ components/enterprise-applications/SamlStatusGrid.data-model.ts
@@ -0,0 +1,36 @@
+export interface GridItem {
+  id: string;
+  appId: string;
+  appDisplayName: string;
+  preferredTokenSigningKeyEndDateTime: string;
+}
+
+export interface SamlStatus {
+  id: string;
+  appId: string;
+  appDisplayName: string;
+  preferredTokenSigningKeyEndDateTime: string;
+}
+
+export function generateSamlStatus(): SamlStatus[] {
+  const now = new Date();
+  const twoMonthsFromNow = new Date();
+  twoMonthsFromNow.setMonth(now.getMonth() + 2);
+  const twoYearsFromNow = new Date();
+  twoYearsFromNow.setFullYear(now.getFullYear() + 2);
+
+  return [
+    {
+      appDisplayName: "Oracle Access Manager for Oracle E-Business Suite",
+      appId: "oracle-id",
+      id: "oracle-id",
+      preferredTokenSigningKeyEndDateTime: twoMonthsFromNow.toISOString(),
+    },
+    {
+      appDisplayName: "AWS Single-Account Access",
+      appId: "aws-id",
+      id: "aws-id",
+      preferredTokenSigningKeyEndDateTime: twoYearsFromNow.toISOString()
+    },
+  ];
+}
