87ceb460b8eb31efe762e6cd5a979291537bf470
Chris Dymond <dwarfered@users.noreply.github.com>
2025-01-03 18:57:22 +0800

    Update msalConfig.ts

--- lib/msalConfig.ts
+++ lib/msalConfig.ts
@@ -54,8 +54,10 @@ export const graphConfig = {
   graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
   graphMePhotoEndpoint:
     "https://graph.microsoft.com/v1.0/me/photos/48x48/$value",
-    graphAppCredentialsEndpoint:
+  graphAppCredentialsEndpoint:
     "https://graph.microsoft.com/v1.0/applications?$select=appId,displayName,keyCredentials,passwordCredentials&$top=999",
+  graphSamlExpiryEndpoint:
+    "https://graph.microsoft.com/beta/servicePrincipals?$select=preferredTokenSigningKeyEndDateTime,appDisplayName,id,appId&$top=999&$filter=preferredTokenSigningKeyEndDateTime ge 2021-01-02T12:00:00Z",
 };
 
 export const msalInstance = new PublicClientApplication(msalConfig);
