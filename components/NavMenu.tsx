a711e0a753589bb68f0b833844b82672eae58ce0
Chris Dymond <dwarfered@users.noreply.github.com>
2025-01-04 08:51:40 +0800

    Update NavMenu.tsx

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -11,10 +11,7 @@ import {
   Toolbar,
   ToolbarGroup,
 } from "@fluentui/react-components";
-import {
-  DividerTallFilled,
-  HomeRegular,
-} from "@fluentui/react-icons";
+import { DividerTallFilled, HomeRegular } from "@fluentui/react-icons";
 import { usePathname, useRouter } from "next/navigation";
 import IconAppRegistrations from "./styling/icons/IconAppRegistrations";
 import IconEnterpriseApps from "./styling/icons/IconEnterpriseApps";
@@ -51,98 +48,115 @@ export default function NavMenu() {
         <Divider />
 
         {/* <AuthenticatedTemplate> */}
-          <Accordion defaultOpenItems="1">
-            <AccordionItem value="1">
-              <AccordionHeader
-                icon={<IconAppRegistrations />}
-                expandIconPosition="end"
+        <Accordion defaultOpenItems="1">
+          <AccordionItem value="1">
+            <AccordionHeader
+              icon={<IconAppRegistrations />}
+              expandIconPosition="end"
+            >
+              <Body1Strong>App Registrations</Body1Strong>
+            </AccordionHeader>
+            <AccordionPanel>
+              <Button
+                icon={
+                  pathname === "/app-registrations/certificates-and-secrets" ? (
+                    <DividerTallFilled
+                      style={{
+                        transform: "scaleX(2)",
+                      }}
+                    />
+                  ) : undefined
+                }
+                onClick={() =>
+                  router.push("/app-registrations/certificates-and-secrets")
+                }
+                shape="square"
+                appearance="subtle"
+                style={{
+                  width: "100%",
+                  justifyContent: "flex-start",
+                  display: "flex",
+                  ...(pathname === "/app-registrations/certificates-and-secrets"
+                    ? activeStyle
+                    : {}),
+                }}
               >
-                <Body1Strong>App Registrations</Body1Strong>
-              </AccordionHeader>
-              <AccordionPanel>
-                <Button
-                  icon={
-                    pathname === "/app-registrations/certificates-and-secrets" ? (
-                      <DividerTallFilled
-                        style={{
-                          transform: "scaleX(2)",
-                        }}
-                      />
-                    ) : undefined
-                  }
-                  onClick={() => router.push("/app-registrations/certificates-and-secrets")}
-                  shape="square"
-                  appearance="subtle"
-                  style={{
-                    width: "100%",
-                    justifyContent: "flex-start",
-                    display: "flex",
-                    ...(pathname === "/app-registrations/certificates-and-secrets" ? activeStyle : {}),
-                  }}
-                >
-                  <Body1>Certificates & secrets</Body1>
-                </Button>
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
+                <Body1>Certificates & secrets</Body1>
+              </Button>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
         {/* </AuthenticatedTemplate> */}
         <Accordion defaultOpenItems="1">
-            <AccordionItem value="1">
-              <AccordionHeader
-                icon={<IconEnterpriseApps />}
-                expandIconPosition="end"
+          <AccordionItem value="1">
+            <AccordionHeader
+              icon={<IconEnterpriseApps />}
+              expandIconPosition="end"
+            >
+              <Body1Strong>Enterprise Applications</Body1Strong>
+            </AccordionHeader>
+            <AccordionPanel>
+              <Button
+                icon={
+                  pathname ===
+                  "/enterprise-applications/app-role-permissions" ? (
+                    <DividerTallFilled
+                      style={{
+                        transform: "scaleX(2)",
+                      }}
+                    />
+                  ) : undefined
+                }
+                onClick={() =>
+                  router.push("/enterprise-applications/app-role-permissions")
+                }
+                shape="square"
+                appearance="subtle"
+                style={{
+                  width: "100%",
+                  justifyContent: "flex-start",
+                  display: "flex",
+                  ...(pathname ===
+                  "/enterprise-applications/app-role-permissions"
+                    ? activeStyle
+                    : {}),
+                }}
               >
-                <Body1Strong>Enterprise Applications</Body1Strong>
-              </AccordionHeader>
-              <AccordionPanel>
-                <Button
-                  icon={
-                    pathname === "/enterprise-applications/saml-certificate-expiry-status" ? (
-                      <DividerTallFilled
-                        style={{
-                          transform: "scaleX(2)",
-                        }}
-                      />
-                    ) : undefined
-                  }
-                  onClick={() => router.push("/enterprise-applications/saml-certificate-expiry-status")}
-                  shape="square"
-                  appearance="subtle"
-                  style={{
-                    width: "100%",
-                    justifyContent: "flex-start",
-                    display: "flex",
-                    ...(pathname === "/enterprise-applications/saml-certificate-expiry-status" ? activeStyle : {}),
-                  }}
-                >
-                  <Body1>SAML certificate status</Body1>
-                </Button>
-                <Button
-                  icon={
-                    pathname === "/enterprise-applications/app-role-assignments" ? (
-                      <DividerTallFilled
-                        style={{
-                          transform: "scaleX(2)",
-                        }}
-                      />
-                    ) : undefined
-                  }
-                  onClick={() => router.push("/enterprise-applications/app-role-assignments")}
-                  shape="square"
-                  appearance="subtle"
-                  style={{
-                    width: "100%",
-                    justifyContent: "flex-start",
-                    display: "flex",
-                    ...(pathname === "/enterprise-applications/app-role-assignments" ? activeStyle : {}),
-                  }}
-                >
-                  <Body1>App role assignments</Body1>
-                </Button>
-              </AccordionPanel>
-            </AccordionItem>
-            
-          </Accordion>
+                <Body1>App role permissions</Body1>
+              </Button>
+              <Button
+                icon={
+                  pathname ===
+                  "/enterprise-applications/saml-certificate-expiry-status" ? (
+                    <DividerTallFilled
+                      style={{
+                        transform: "scaleX(2)",
+                      }}
+                    />
+                  ) : undefined
+                }
+                onClick={() =>
+                  router.push(
+                    "/enterprise-applications/saml-certificate-expiry-status"
+                  )
+                }
+                shape="square"
+                appearance="subtle"
+                style={{
+                  width: "100%",
+                  justifyContent: "flex-start",
+                  display: "flex",
+                  ...(pathname ===
+                  "/enterprise-applications/saml-certificate-expiry-status"
+                    ? activeStyle
+                    : {}),
+                }}
+              >
+                <Body1>SAML certificate status</Body1>
+              </Button>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </ToolbarGroup>
       <ToolbarGroup role="presentation"></ToolbarGroup>
     </Toolbar>
