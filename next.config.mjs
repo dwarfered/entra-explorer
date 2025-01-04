04450386800539bcdd7d2f91ce60ac13f4dd4768
Chris Dymond <dwarfered@users.noreply.github.com>
2025-01-04 21:46:45 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // static site or Single-Page Application (SPA),
+    images: { unoptimized: true },
     reactStrictMode: false, // https://github.com/microsoft/fluentui/issues/31930
 };
 
