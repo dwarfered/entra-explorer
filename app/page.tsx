8cb0f8fb322780fbb36de4b34c6eebc7368b0664
Chris Dymond <dwarfered@users.noreply.github.com>
2025-01-05 06:48:24 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,10 +22,6 @@ export default function Home() {
           Microsoft Entra.
         </p>
       </Body1>
-      <Caption1>
-        Please note that Entra Explorer is not an official Microsoft product and
-        is in no way affiliated with, endorsed by, or sponsored by Microsoft.
-      </Caption1>
 
       <h2>Solution Overview</h2>
 
@@ -44,16 +40,16 @@ export default function Home() {
       </div>
 
       <p>
-        Entra Explorer is a single-page application (SPA) hosted on GitHub
-        Pages, built with Next.js (a React framework) and styled using
-        Microsoft&apos;s Fluent UI. It leverages the Microsoft Authentication Library
-        (MSAL) for authentication, with all Microsoft Graph requests handled on
-        the client side.
+        The solution is a single-page application (SPA) hosted on GitHub Pages,
+        built with Next.js (a React framework) and styled using Microsoft&apos;s
+        Fluent UI. It leverages the Microsoft Authentication Library (MSAL) for
+        authentication, with all Microsoft Graph requests handled on the client
+        side.
       </p>
 
       <div>
         <Caption1>
-          GitHub Source:{" "}
+          GitHub:{" "}
           <Link
             target="_blank"
             href="https://github.com/dwarfered/entra-explorer"
@@ -78,23 +74,31 @@ export default function Home() {
 
       <Divider />
 
+      <p>
+        <Caption1>
+          Please note that Entra Explorer is not an official Microsoft product
+          and is in no way affiliated with, endorsed by, or sponsored by
+          Microsoft.
+        </Caption1>
+      </p>
+
       <Caption2>
         MIT License Copyright (c) 2024 Chris Dymond Permission is hereby
         granted, free of charge, to any person obtaining a copy of this software
-        and associated documentation files (the &quot;Software&quot;), to deal in the
-        Software without restriction, including without limitation the rights to
-        use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions: The above
-        copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED
-        &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-        NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-        PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-        COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-        WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
-        OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-        THE SOFTWARE.
+        and associated documentation files (the &quot;Software&quot;), to deal
+        in the Software without restriction, including without limitation the
+        rights to use, copy, modify, merge, publish, distribute, sublicense,
+        and/or sell copies of the Software, and to permit persons to whom the
+        Software is furnished to do so, subject to the following conditions: The
+        above copyright notice and this permission notice shall be included in
+        all copies or substantial portions of the Software. THE SOFTWARE IS
+        PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+        THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+        OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+        ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+        OTHER DEALINGS IN THE SOFTWARE.
       </Caption2>
     </main>
   );
